d9afeebd26596e7e8658805d6fe417636e7ac039
Tochka <tochka114@gmail.com>
2024-07-27 23:51:36 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Description from './components/Description/Description'
+import Options from './components/Options/Options'
+
+function App() {
+  
+
+  return (
+    <>
+      <Description/>
+      <Options/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,15 @@
+
+
+const Options = () => {
+  return (
+    <div>
+<button>Good</button>
+<button>Neutral</button>
+
+<button>Bad</button>        
+            
+        
+    </div>
+  )
+}
+export default Options
