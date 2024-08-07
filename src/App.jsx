bab2b60a3bd85ffd64f5a59977b6078725b9835c
Tochka <tochka114@gmail.com>
2024-08-07 14:58:30 +0300

    the end

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
+import './App.css';
+import Description from './components/Description/Description';
+import Feedback from './components/Feedback/Feedback';
+import Options from './components/Options/Options';
+import Notification from './components/Notification/Notification';
+import { useState, useEffect } from 'react';
 
-import './App.css'
-import Description from './components/Description/Description'
-import  Feedback  from './components/Feedback/Feedback'
-import Options from './components/Options/Options'
-import Notification from './components/Notification/Notification'
-import { useState } from 'react'
 function App() {
-  const [feedback,setFeedback]=useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  })
+  const [feedback, setFeedback] = useState(() => {
+    const feedbackLocalStorage = window.localStorage.getItem('feedback');
+    try {
+      return feedbackLocalStorage ? JSON.parse(feedbackLocalStorage) : {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      };
+    } catch (e) {
+      return {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      };
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
   const updateFeedback = (feedbackType) => {
-    setFeedback({...feedback, [feedbackType]:feedback[feedbackType]+1})
+    setFeedback({...feedback, [feedbackType]: feedback[feedbackType] + 1});
   };
+
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback=Math.round((feedback.good / totalFeedback) * 100)
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100) || 0;
+
   const resetFeedback = () => {
     setFeedback({
       good: 0,
@@ -23,26 +41,27 @@ function App() {
       bad: 0,
     });
   };
+
   return (
     <>
-      <Description/>
-      <Options 
-      updateFeedback={updateFeedback}
-      resetFeedback={resetFeedback}
-      totalFeedback={totalFeedback}
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
       />
-      {totalFeedback===0 &&<Notification/>}
-      {totalFeedback>0 && <Feedback
-      good={feedback.good}
-      neutral={feedback.neutral}
-      bad={feedback.bad}
-    totalFeedback = {totalFeedback}
-    positiveFeedback={positiveFeedback}
-      
-      />}  
-      
+      {totalFeedback === 0 && <Notification />}
+      {totalFeedback > 0 && (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
